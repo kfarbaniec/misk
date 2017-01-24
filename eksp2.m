fea8d72856af61626aeac087c362646f6eedb478
kfarbaniec <farbaniec.karol@gmail.com>
2017-01-24 08:25:08 +0100

    .

---
+++ eksp2.m
@@ -0,0 +1,16 @@
+%% 2
+%
+% PL: Wyznaczenie hipotetycznego modelu matematycznego 
+% mechaniki robota w celu wyznaczenia częstotliwości drgań własnych układu 
+% oraz symulacja wartości parametrów kinematycznych 
+% na wymuszenie modelujące przejazd.
+%
+% ENG: Mathematical modeling mechanical system of robot to find natural
+% freaquencies of system. Analysis of kinematic parameters in time and frequency domain i.e.
+% displacement, velocity and acceleration as a response for input signal -
+% simulation of ride.
+%
+%
+%
+
+
